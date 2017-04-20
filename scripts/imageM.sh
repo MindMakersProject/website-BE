fc61b28ac24a704229929b5f9d194694653d5488
wcjohnson11 <williamcouperjohnson@gmail.com>
2017-04-20 10:03:01 -0700

    update intro to programming

--- scripts/imageM.sh
+++ scripts/imageM.sh
@@ -4,8 +4,8 @@ do
   echo "$f"
   test=`convert $f -format "%[fx:(w/h>1)?1:0]" info:`
   if [ $test -eq 1 ]; then
-  convert $f -resize 500x $f
+  convert $f -resize 500x -quality 95 $f
   else
-  convert $f -resize x500 -quality 75 $f
+  convert $f -resize x500 -quality 95 $f
   fi
 done
