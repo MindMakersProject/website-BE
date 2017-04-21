787b05c264f8e37cb08a364af05b023021d4ea4e
wcjohnson11 <williamcouperjohnson@gmail.com>
2017-04-21 10:31:47 -0700

    updating images with new imagemagick script

---
+++ scripts/outreachSliderImagePrep.sh
@@ -0,0 +1,9 @@
+FILES=./*.jpg
+for f in $FILES
+do 
+  echo "$f"
+  convert $f -resize 500x500! -fill 'rgba(170, 130, 255, 0.8)' -colorize 80% background.jpg
+  convert $f -resize 500x500 overlay.jpg
+  composite -gravity center overlay.jpg background.jpg $f
+  rm overlay.jpg && background.jpg
+done
